af0a8a881984a0fdc51536972778ba8728e15f7f
Guilherme Spro <guilherme.spro@gmail.com>
2020-08-12 17:21:13 -0300

    blockcountdown2

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
 
 interface CountdownProps {
   targetDate: string;
@@ -7,12 +10,19 @@ interface CountdownProps {
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   targetDate,
 }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  }
+  tick(targetDate, setTime)
   return (
     <div>
-      <h1>{targetDate}</h1>
+     <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
     </div>
   );
 };
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
 
 Countdown.schema = {
   title: "editor.countdown.title",
