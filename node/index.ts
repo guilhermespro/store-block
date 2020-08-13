782c9422276aae0c01c6b90890cf5fdfbdac712d
Guilherme Spro <guilherme.spro@gmail.com>
2020-08-13 15:49:04 -0300

    events

--- node/index.ts
+++ node/index.ts
@@ -1,10 +1,13 @@
 import { ClientsConfig, LRUCache, Service, ServiceContext } from '@vtex/api'
-
+import { updateLiveUsers } from './event/liveUsersUpdate'
 import { Clients } from './clients'
 
+
+const TREE_SECONDS_MS = 3 * 1000
+const CONCURRENCY = 10
 const TIMEOUT_MS = 5000
 
-const memoryCache = new LRUCache<string, any>({max: 5000})
+const memoryCache = new LRUCache<string, any>({ max: 5000 })
 metrics.trackCache('status', memoryCache)
 
 const clients: ClientsConfig<Clients> = {
@@ -25,12 +28,22 @@ declare global {
 }
 
 // Export a service that defines route handlers and client options.
-export default new Service<Clients, {}>({
-  clients,
-  graphql: {
-    resolvers: {
-      Query: {
-
+export default new Service<Clients, State, ParamsContext>({
+  clients: {
+    implementation: Clients,
+    options: {
+      default: {
+        retries: 2,
+        timeout: 10000,
+      },
+      events: {
+        liveUsersUpdate: updateLiveUsers,
+        exponentialTimeoutCoefficient: 2,
+        exponentialBackoffCoefficient: 2,
+        initialBackoffDelay: 50,
+        retries: 1,
+        timeout: TREE_SECONDS_MS,
+        concurrency: CONCURRENCY,
       },
     },
   },
